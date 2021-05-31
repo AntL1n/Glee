b5ef3f7ac12215f648113805f7fcb97b08af955d
https://github.com/AntL1n <antlin96@gmail.com>
2021-06-01 00:20:08 +0300

    add products and design

--- app/js/index.js
+++ app/js/index.js
@@ -3,4 +3,16 @@ $(function () {
         dots: true,
         arrows: false,
     });
+
+    
+    let containerEl1 = document.querySelector('.products');
+    let containerEl2 = document.querySelector('.design');
+    let config = {
+        controls: {
+            scope: 'local',
+        },
+    };
+    (() => mixitup(containerEl1, config))();
+    (() => mixitup(containerEl2, config))();
 });
+
--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ const scripts = () => {
     return src([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/mixitup/dist/mixitup.js',
         'app/js/index.js',
     ])
         .pipe(sourcemaps.init())

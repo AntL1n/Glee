91d98f9003b9b4def49e8070d3bacfa37c3e11f4
AntL1n <antlin96@gmail.com>
2021-06-16 22:56:31 +0300

    adaptive header and footer

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,9 @@
 $(function () {
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active');
+        $('.menu__btn').toggleClass('menu__btn--active');
+    });
+
     $('.top-slider__inner').slick({
         dots: true,
         arrows: false,
